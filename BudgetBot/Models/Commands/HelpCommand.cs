d7001900d583fb384c7d98121668993ea5c5ed32
Іван Захарук <zaharuck@outlook.com>
2020-03-06 13:31:37 +0200

    Implemented help command

---
+++ BudgetBot/Models/Commands/HelpCommand.cs
@@ -0,0 +1,29 @@
+﻿using BudgetBot.Models.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace BudgetBot.Models.Commands
+{
+    public class HelpCommand : BaseCommand
+    {
+        public override string Name { get => "/help"; }
+
+        public override async Task Execute(Update update, TelegramBotClient client)
+        {
+            var answer = new Emoji(0x2139).ToString() + " Вас вітає Budget_bot) \n" +
+                "Цей бот допоможе вам слідкувати за доходами та витратими. \n" +
+                "Щоб почати роботу скористайтесь доступними командами: \n" +
+                "/addexpense - додати витрату\n" +
+                "/addrevenue - додати дохід\n" +
+                "/getexpensestat - статистика витрат\n" +
+                "/getrevenuestat - статистика доходів\n" +
+                "/balance - баланс\n" +
+                "/addcategory - додати категорію витрат чи доходів";
+            await client.SendTextMessageAsync(update.Message.Chat.Id, answer);
+        }
+    }
+}
